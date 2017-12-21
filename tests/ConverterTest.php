1c1b62fb83be94af24494c031803c4253535f1ab
sven <sven@wysiwyg.de>
2017-12-21 15:56:53 +0100

    Added Converter Extension Tests

--- tests/ConverterTest.php
+++ tests/ConverterTest.php
@@ -8,13 +8,35 @@ class ConverterTest extends TestCase
 {
     /**
      * @test
+     * @expectedException \Breadlesscode\Office\Exception\ConverterException
+     */
+    public function converterCanDetectUnsupportedExtension()
+    {
+        self::assertFalse(Converter::canHandleExtension('php'));
+
+        Converter::file($this->getTestFile('Test.php'));
+        self::expectException(\Breadlesscode\Office\Exception\ConverterException::class);
+    }
+
+    /**
+     * @test
+     */
+    public function converterCanConvertOdtFile()
+    {
+        $fileConverter = Converter::file($this->getTestFile('Test.odt'));
+        self::assertTrue(Converter::canHandleExtension('odt'));
+        self::assertInstanceOf(Converter::class, $fileConverter);
+    }
+
+    /**
+     * @test
      */
     public function canGenerateThumbnail()
     {
         Converter::file($this->getTestFile('Test.odt'))
             ->thumbnail();
 
-        $this->assertFileExists($this->getTestFileDirectory().'/Test.jpg');
+        $this->assertFileExists($this->getTestFileDirectory() . '/Test.jpg');
     }
 
     /**
@@ -25,7 +47,7 @@ class ConverterTest extends TestCase
         Converter::file($this->getTestFile('Test.odt'))
             ->save('MyTest.pdf');
 
-        $this->assertFileExists($this->getTestFileDirectory().'/Test.jpg');
+        $this->assertFileExists($this->getTestFileDirectory() . '/Test.jpg');
     }
 
     /**
